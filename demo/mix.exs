1e299994e052aa6387479bf31021f2ca8a78c251
Takuya <yky2800.rthm.55tn@gmail.com>
2019-09-01 10:02:17 +0900

    JOKENを入れてみる

--- demo/mix.exs
+++ demo/mix.exs
@@ -41,9 +41,10 @@ defmodule Demo.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
+      {:jason, "~> 1.1"},
       {:plug_cowboy, "~> 2.0"},
-      {:poison, "~> 4.0"}
+      {:poison, "~> 4.0"},
+      {:joken, "~> 2.0"}
     ]
   end
 
